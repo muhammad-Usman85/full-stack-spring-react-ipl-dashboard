d26739f7a1cdb749d68f950164bd22dc15860064
Muhammad Usman <muhammad.u@royalcyber.com>
2021-06-16 15:16:06 +0500

    init design

--- cricket-client/src/pages/TeamPage.js
+++ cricket-client/src/pages/TeamPage.js
@@ -1,9 +1,16 @@
 import { React } from 'react'
+import { MatchDetailCard } from '../components/MatchDetailCard';
+import { MatchSmallCard } from '../components/MatchSmallCard';
 
 export const TeamPage = () => {
   return (
     <div className="TeamPage">
-      <h1>this is team Page</h1>
+      <h1>Team Name</h1>
+      <MatchDetailCard />
+      <MatchSmallCard />
+      <MatchSmallCard />
+      <MatchSmallCard />
+      <MatchSmallCard />
     </div>
   );
 }
